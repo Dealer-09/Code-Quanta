7417798b7a6476d5a2e0ed0c52581ad1eb5ea1f8
Archis <archismanpaul09@outlook.com>
2025-05-31 19:02:41 +0530

    Firebase Setup & Sign Up Page Completed

--- app/src/main/java/com/Archis/code_quanta/SignUp.java
+++ app/src/main/java/com/Archis/code_quanta/SignUp.java
@@ -3,23 +3,45 @@ package com.Archis.code_quanta;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignUp extends AppCompatActivity {
+
+    public static final String TAG="TAG";
+
     EditText userName, userEmail, userPhone, password;
     Button register;
     TextView signIn;
     ProgressBar progressBar;
+    FirebaseAuth mAuth;
+    FirebaseFirestore mStore;
+    FirebaseUser mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +60,15 @@ public class SignUp extends AppCompatActivity {
         register= findViewById(R.id.register);
         progressBar= findViewById(R.id.progressBar);
         signIn= findViewById(R.id.signIn);
+        mAuth= FirebaseAuth.getInstance();
+        mStore= FirebaseFirestore.getInstance();
+
+        if (mAuth.getCurrentUser()!=null)
+        {
+            startActivity(new Intent(SignUp.this, MainActivity.class));
+            finishAffinity();
+        }
+
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,6 +106,48 @@ public class SignUp extends AppCompatActivity {
                     return;
                 }
                 progressBar.setVisibility(View.VISIBLE);
+
+                mAuth.createUserWithEmailAndPassword(mEmail,mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            FirebaseUser mUser= mAuth.getCurrentUser();
+                            mUser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(SignUp.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG,"onFailure: Email Not Sent"+e.getMessage());
+                                }
+                            });
+                            DocumentReference documentReference= mStore.collection("users").document(mUser.getUid());
+                            Map<String, Object> user= new HashMap<>();
+                            user.put("Name",mUserName);
+                            user.put("Email",mEmail);
+                            user.put("PhNumber",mPhone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG,"User Profile Created");
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG,"On Failure: "+e.getMessage());
+                                }
+                            });
+                            Toast.makeText(SignUp.this, "User Registered", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SignUp.this, MainActivity.class));
+                            finishAffinity();
+                        }else {
+                            Toast.makeText(SignUp.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
