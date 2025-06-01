d5a83ea2e29bab3e3179adc3ecf9b86d08ab863f
Archis <archismanpaul09@outlook.com>
2025-06-01 23:32:57 +0530

    Sign In Page Firebase Connected

--- app/src/main/java/com/Archis/code_quanta/SignIn.java
+++ app/src/main/java/com/Archis/code_quanta/SignIn.java
@@ -10,20 +10,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignIn extends AppCompatActivity {
 
     EditText userEmail, password;
-    Button signIn;
+    Button signInBtn;
     TextView signUp;
     ProgressBar progressBar;
-
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,9 +43,10 @@ public class SignIn extends AppCompatActivity {
         });
         userEmail= findViewById(R.id.userEmail);
         password= findViewById(R.id.password);
-        signIn= findViewById(R.id.signIn);
+        signInBtn= findViewById(R.id.signIn);
         signUp= findViewById(R.id.signUp);
         progressBar= findViewById(R.id.progressBar);
+        mAuth= FirebaseAuth.getInstance();
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,7 +54,7 @@ public class SignIn extends AppCompatActivity {
                 finish();
             }
         });
-        signIn.setOnClickListener(new View.OnClickListener() {
+        signInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String mEmail=userEmail.getText().toString().trim();
@@ -59,8 +67,21 @@ public class SignIn extends AppCompatActivity {
                     password.setError("Please enter your password");
                     return;
                 }
-
                 progressBar.setVisibility(VISIBLE);
+                mAuth.signInWithEmailAndPassword(mEmail,mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(SignIn.this, "User Logged In Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SignIn.this, MainActivity.class));
+                            finishAffinity();
+                        }else {
+                            Toast.makeText(SignIn.this, "Check Your Email and Password", Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
             }
         });
 
