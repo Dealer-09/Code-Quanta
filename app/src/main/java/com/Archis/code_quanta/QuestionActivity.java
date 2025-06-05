1831a9fbbd49f4558872c0222e54e4695b509311
Archis <archismanpaul09@outlook.com>
2025-06-06 02:10:10 +0530

    Question Page Temporary

--- app/src/main/java/com/Archis/code_quanta/QuestionActivity.java
+++ app/src/main/java/com/Archis/code_quanta/QuestionActivity.java
@@ -1,54 +1,24 @@
 package com.Archis.code_quanta;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
+public class QuestionActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_question);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        toolbar=findViewById(R.id.toolbar);
-    }
-
-    public void java(View view) {
-        startActivity(new Intent(MainActivity.this,QuestionActivity.class));
-        finish();
-    }
-
-    public void kotlin(View view) {
-    }
-
-    public void python(View view) {
-    }
-
-    public void javascript(View view) {
-    }
-
-    public void c(View view) {
-    }
-
-    public void cpp(View view) {
-    }
-
-    public void dart(View view) {
-    }
-
-    public void rust(View view) {
     }
 }
